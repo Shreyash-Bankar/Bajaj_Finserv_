409a0807d85f66890b986b964c2cdc123a6a7a79
Shreyash-Bankar <156165905+Shreyash-Bankar@users.noreply.github.com>
2025-08-29 11:23:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-
+//Shreyash Bankar 22BCI0046
 import express from "express";
 
 const app = express();
 app.use(express.json());
-
-const FULL_NAME = "shreyash_bankar";
-const DOB = "22032004"; // ddmmyyyy
-const EMAIL = "shreyash@example.com";
-const ROLL_NUMBER = "22BCI0046";
-
-
-function alternatingCaps(str) {
+const FULL_NAME ="Shreyash_Bankar";
+const DOB= "07092003"; 
+const EMAIL ="shreyashbankar10@gmail.com";
+const ROLL_NUMBER = "22BCI0046";function alternatingCaps(str) {
   let result = "";
   for (let i = 0; i < str.length; i++) {
     result += i % 2 === 0 ? str[i].toUpperCase() : str[i].toLowerCase();
   }
-  return result;
+ return result;
 }
 
 app.post("/bfhl", (req, res) => {
   try {
-    const { data } = req.body;
+    const {data } = req.body;
 
     if (!Array.isArray(data)) {
       return res.status(400).json({
@@ -29,17 +25,17 @@ app.post("/bfhl", (req, res) => {
       });
     }
 
-    let even_numbers = [];
-    let odd_numbers = [];
-    let alphabets = [];
+    let even_numbers=[]; 
+    let odd_numbers = [] ;
+    let alphabets =[];
     let special_characters = [];
-    let sum = 0;
+    let sum = 0 ;
 
     data.forEach((item) => {
       // Check if item is a number (pure digit string)
-      if (/^-?\d+$/.test(item)) {
+      if ( /^-?\d+$/.test(item)){
         let num = parseInt(item);
-        if (num % 2 === 0) even_numbers.push(item);
+        if (num % 2 === 0) even_numbers.push(item );
         else odd_numbers.push(item);
         sum += num;
       }
@@ -47,25 +43,23 @@ app.post("/bfhl", (req, res) => {
       else if (/^[a-zA-Z]+$/.test(item)) {
         alphabets.push(item.toUpperCase());
       }
-      // Otherwise → special character
       else {
         special_characters.push(item);
       }
     });
 
-    // Concatenation logic
+
     let concatenated = alphabets
       .map((a) => a.toLowerCase())
       .reverse()
       .join("");
     let concat_string = alternatingCaps(concatenated);
-
-    res.status(200).json({
+  res.status(200).json({
       is_success: true,
       user_id: `${FULL_NAME}_${DOB}`,
       email: EMAIL,
       roll_number: ROLL_NUMBER,
-      odd_numbers,
+     odd_numbers,
       even_numbers,
       alphabets,
       special_characters,
@@ -82,4 +76,5 @@ app.post("/bfhl", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+
 
